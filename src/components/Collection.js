7704cde958e44cbf9a1c03c84412f2fac70fee4a
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-23 22:48:59 +0430

    Style collection

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Collection = () => (
-  <div>
+  <div className="col-6 col-md-4 col-lg-3 light-pink text-center">
     <h2>Name of the Collection</h2>
     <h3>Images: 30</h3>
     <h3>Videos: 45</h3>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { FaAngleLeft, FaMicrophone, FaCog } from 'react-icons/fa';
 const MyNavbar = () => (
   <nav className="navbar pink">
     <FaAngleLeft className="ms-3" />
-    <h1 className="col-10 fs-4">Galleries</h1>
+    <h1 className="col-9 fs-4">Galleries</h1>
     <FaMicrophone className="me-0" />
     <FaCog className="ms-2 me-3" />
   </nav>
