338e5e73ee092b68b2ef3455c8702bbffef51a1c
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-23 21:48:47 +0430

    Add icons to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const Navbar = () => (<div>Navbar</div>);
+import { FaAngleLeft, FaMicrophone, FaCog } from 'react-icons/fa';
+
+const Navbar = () => (
+  <div>
+    <FaAngleLeft />
+    <h1>Galleries</h1>
+    <FaMicrophone />
+    <FaCog />
+  </div>
+);
 
 export default Navbar;
