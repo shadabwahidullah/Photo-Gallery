8cc273535f8c240581873dd7e867d9746a07ee5b
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-25 02:29:11 +0430

    Add search for the home page

--- src/components/home/Search.js
+++ src/components/home/Search.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { filterCollectionsAction } from '../../redux/reducers/collections';
 
-const Search = () => (
-  <div className="row">
-    <form>
-      <input className="form-control m-2" placeholder="Search for images" />
-    </form>
-  </div>
-);
+const Search = () => {
+  const dispatch = useDispatch();
+  const filter = (event) => {
+    dispatch(filterCollectionsAction(event.target.value));
+  };
 
+  return (
+    <div className="row">
+      <form>
+        <input
+          className="form-control m-2"
+          placeholder="Search for images"
+          onChange={(event) => {
+            console.log('the typed in message is ', event.target.value);
+            filter(event);
+          }}
+        />
+      </form>
+    </div>
+  );
+};
 export default Search;
--- src/redux/reducers/collections.js
+++ src/redux/reducers/collections.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const FETCH_COLLECTIONS = 'photogallery/collections/FETCH_COLLECTIONS';
+const FILTER_COLLECTIONS = 'photogallery/collections/FILTER_COLLECTIONS';
 
 const BASE_URL = 'https://api.pexels.com/v1/collections';
 const FEATURED_COLLECTIONS = `${BASE_URL}/featured?per_page=25`;
@@ -19,13 +20,25 @@ export const fetchCollections = () => (dispatch) => {
     });
 };
 
+export const filterCollectionsAction = (value) => (dispatch) => {
+  console.log('filter collections called with value: ', value);
+  dispatch({ type: FILTER_COLLECTIONS, payload: value });
+};
+
 const initialState = { Collections: [] };
 
 const collectionsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_COLLECTIONS:
       console.log('inside the switch', action.payload);
-      return { ...state, Collections: action.payload };
+      return { ...state, Collections: action.payload, collections: action.payload };
+    case FILTER_COLLECTIONS:
+      console.log('filtered collection');
+      return {
+        ...state,
+        Collections: state.collections.filter((collection) => collection.title
+          .toLowerCase().includes(action.payload.toLowerCase())),
+      };
     default:
       return state;
   }
