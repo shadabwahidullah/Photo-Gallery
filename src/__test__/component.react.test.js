2b9569e78e40d47b22615a29fa401cb1d847a258
shadabwahidullah <shadabwahidullah@gmail.com>
2021-11-26 09:35:21 +0430

    Resolve eslint errors

--- src/__test__/component.react.test.js
+++ src/__test__/component.react.test.js
@@ -1,69 +1,68 @@
-import React from "react";
-import { Provider } from "react-redux";
-import renderer from "react-test-renderer";
-import store from "../redux/configureStore";
-import Home from "../components/home/Home";
-import Search from "../components/home/Search";
-import Banner from "../components/Banner";
-import Collection from "../components/Collection";
-import TargetCollection from "../components/target_collection/TargetCollection";
-import Photo from "../components/target_collection/media/Photo";
-import { Navbar } from "react-bootstrap";
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import { Navbar } from 'react-bootstrap';
+import store from '../redux/configureStore';
+import Home from '../components/home/Home';
+import Search from '../components/home/Search';
+import Banner from '../components/Banner';
+import Collection from '../components/Collection';
+import Photo from '../components/target_collection/media/Photo';
 
-describe("Components snapshot testing", () => {
-  test("Home page testing", () => {
+describe('Components snapshot testing', () => {
+  test('Home page testing', () => {
     const homePage = renderer.create(
       <Provider store={store}>
         <Home />
-      </Provider>
+      </Provider>,
     ).toJSON;
     expect(homePage).toMatchSnapshot();
   });
-  test("Search testing", () => {
+  test('Search testing', () => {
     const search = renderer.create(
       <Provider store={store}>
         <Search />
-      </Provider>
+      </Provider>,
     ).toJSON;
     expect(search).toMatchSnapshot();
   });
-  test("Banner testing", () => {
+  test('Banner testing', () => {
     const banner = renderer.create(
       <Provider store={store}>
         <Banner />
-      </Provider>
+      </Provider>,
     ).toJSON;
     expect(banner).toMatchSnapshot();
   });
-  test("Collection testing", () => {
-    const test = { id: 122, title: "testing title", photos_count: "1239" };
+  test('Collection testing', () => {
+    const test = { id: 122, title: 'testing title', photos_count: '1239' };
 
     const collection = renderer.create(
       <Provider store={store}>
         <Collection collectionInfo={test} />
-      </Provider>
+      </Provider>,
     ).toJSON;
     expect(collection).toMatchSnapshot();
   });
-  test("Photo testing", () => {
+  test('Photo testing', () => {
     const testPhoto = {
-      id: "test",
-      photographer: "testPhotographer",
-      src: { portrait: "test source", medium: "test source" },
-      height: "200",
+      id: 'test',
+      photographer: 'testPhotographer',
+      src: { portrait: 'test source', medium: 'test source' },
+      height: '200',
     };
     const photo = renderer.create(
       <Provider store={store}>
         <Photo photo={testPhoto} />
-      </Provider>
+      </Provider>,
     ).toJSON;
     expect(photo).toMatchSnapshot();
   });
-  test("Navbar testing", () => {
+  test('Navbar testing', () => {
     const navbar = renderer.create(
       <Provider store={store}>
         <Navbar />
-      </Provider>
+      </Provider>,
     ).toJSON;
     expect(navbar).toMatchSnapshot();
   });
